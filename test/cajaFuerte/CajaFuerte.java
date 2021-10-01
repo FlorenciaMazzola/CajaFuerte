d31ada668ccc281e0ad8c931aad571b508452810
Florencia Mazzola <mflorenciamazzola@gmail.com>
2021-10-01 18:44:29 -0300

    Estado inicial del proyecto

---
+++ test/cajaFuerte/CajaFuerte.java
@@ -0,0 +1,14 @@
+package cajaFuerte;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CajaFuerte {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
